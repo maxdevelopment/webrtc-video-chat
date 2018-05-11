b775835c6e210115f5a374155a4f93254572146b
Maksim Naranovich <maksim.naranovich@oxagile.com>
2018-05-11 13:21:25 +0300

    path for socket and api

--- client/src/static/routes.js
+++ client/src/static/routes.js
@@ -1,19 +1,19 @@
-const basePath = 'http://127.0.0.1:3000'
+const basePath = window.location.hostname === 'rtc' ? 'http://127.0.0.1:3000' : 'http://5.61.45.181:3000'
 
 const wsSchema = window.location.protocol === 'http:' ? 'ws:' : 'wss:'
-const baseSocket = `${wsSchema}//127.0.0.1:3000`
+const baseSocket = window.location.hostname === 'rtc' ? `${wsSchema}//127.0.0.1:3000` : `${wsSchema}//5.61.45.181:3000`
 
 export default {
-  createRoomPath: () => (
-    `${basePath}/api/create_room`
-  ),
-  getRoomPath: slug => (
-    `${basePath}/api/get_room/${slug}`
-  ),
-  connectToRoomPath: slug => (
-    `${basePath}/api/room/${slug}`
-  ),
-  socketPath: slug => (
-    `${baseSocket}/channel/${slug}`
-  )
+    createRoomPath: () => (
+        `${basePath}/api/create_room`
+    ),
+    getRoomPath: slug => (
+        `${basePath}/api/get_room/${slug}`
+    ),
+    connectToRoomPath: slug => (
+        `${basePath}/api/room/${slug}`
+    ),
+    socketPath: slug => (
+        `${baseSocket}/channel/${slug}`
+    )
 }
